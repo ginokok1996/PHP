1581418d3f7a2df875c6f4b2dc7847cb27b097a9
ginokok1996 <ginokok1996@hotmail.com>
2020-04-08 14:02:24 +0200

    hoofdstuk 5 deels klaar (overstap naar visual studio code proberen)

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
     <a href="h02/hoofdstuk2.php">Hoofdstuk 2</a>
     <a href="h03/hoofdstuk3.php">Hoofdstuk 3</a>
     <a href="h04/hoofdstuk4.php">Hoofdstuk 4</a>
+    <a href="h05/hoofdstuk5.php">Hoofdstuk 5</a>
 
 
 
