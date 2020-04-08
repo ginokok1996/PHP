9dca9286c45ddb24b509642938908ff8938fca07
ginokok1996 <ginokok1996@hotmail.com>
2020-04-08 13:16:28 +0200

    hoofdstuk 4 toegevoegd

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
     <a href="h01/hoofdstuk1.php">Hoofdstuk 1</a>
     <a href="h02/hoofdstuk2.php">Hoofdstuk 2</a>
     <a href="h03/hoofdstuk3.php">Hoofdstuk 3</a>
+    <a href="h04/hoofdstuk4.php">Hoofdstuk 4</a>
+
 
 
 </body>
